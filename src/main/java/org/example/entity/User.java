fb994f4dbc7b9f4d8e71e72513e29e91f6e17219
sdlxnvv <murodsadulloxonov@yandex.ru>
2025-04-07 18:04:07 +0500

    User class

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,20 @@
+package org.example.entity;
+
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString(exclude = "password")
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private UUID id;
+    private String fullName;
+    private String email;
+    private String password;
+    private Boolean active;
+}
