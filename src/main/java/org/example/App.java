19a734f3a0799fa534e2e5a785b343890893b845
sdlxnvv <murodsadulloxonov@yandex.ru>
2025-04-14 02:37:26 +0500

    AppJava is changed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,32 +8,28 @@ import javax.mail.MessagingException;
 
 public class App {
     static UserService userService = new UserService();
-    public static void main( String[] args ) throws MessagingException {
+
+    public static void main(String[] args) throws MessagingException {
         boolean isExit = false;
 
         while (!isExit) {
 
             System.out.print("""
-            1. login
-            2. register
-
-            0. exit
-            =>\s""");
+                    1. login
+                    2. register
+                    
+                    0. exit
+                    =>\s""");
 
 
             switch (DB.scannerInt.nextInt()) {
-                case 1:
-                    userService.login();
-                    break;
-                case 2:
-                    userService.register();
-                    break;
-                case 0:
-                    isExit = true;
-                    break;
-                default:
-                    System.out.println("Error 404");
-                    break;
+                case 1 -> userService.login();
+                case 2 -> userService.register();
+                case 0 -> isExit = true;
+                default ->{
+                    System.out.println("Wrong option");
+                    return;
+                }
             }
         }
     }
